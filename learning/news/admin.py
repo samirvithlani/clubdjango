f63d1b7c7c85581fbbda91dff02454572959bd34
samirvithlani <samir.vithlani83955@gmail.com>
2022-05-30 17:42:04 +0530

    modles foreign key

--- learning/news/admin.py
+++ learning/news/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import News
+from .models import *
 
 # Register your models here.
 
 admin.site.register(News)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Course)
+admin.site.register(Student)
--- learning/news/models.py
+++ learning/news/models.py
@@ -14,3 +14,48 @@ class News(models.Model):
     class Meta:
         db_table = 'news'
 
+class Author(models.Model):
+    #id
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200,unique=True)
+    age = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'authors'
+
+class Book(Author):
+    bookName = models.CharField(max_length=200)
+    bookprice = models.FloatField(null=False)
+    publishDate = models.DateTimeField('date published')
+
+    class Meta:
+        db_table = 'books'
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'courses'
+
+
+class Student(models.Model):
+    cource = models.ForeignKey(Course,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'students'
+
+
+class Abc(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(null=False)
+
+
+
+
+
+
+
+
