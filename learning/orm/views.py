492bfd269db15db3c94c60c9b556381c4f929371
samirvithlani <samir.vithlani83955@gmail.com>
2022-06-06 17:45:31 +0530

    orm lookups

--- learning/orm/views.py
+++ learning/orm/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
+from django.db.models import *
+
 
 from .models import Student
 
 # Create your views here.
 def getStudentData(request):
     studentList = Student.objects.all().values()
-    print(studentList)
+    students = Student.objects.filter(name__startswith='h')
+    students1 = Student.objects.filter(email__endswith='.com').values()
+    #students2 = Student.objects.filter
+    students3 = Student.objects.filter(age__gte=20).values()
+    student4 = Student.objects.filter(password__contains='@').values()
+    students5 = Student.objects.filter(id__exact=1).values()
+    students6 = Student.objects.filter(rollno__range=(8,10)).values()
+    students7 = Student.objects.filter(rollno__range=(1,10)).count()
 
-    return render(request,"orm/studentList.html",{'studentList':studentList})
+    students8 = Student.objects.all().aggregate(Sum('age'))
+    students9 = Student.objects.all().aggregate(Avg('age'))
+    students10 = Student.objects.all().aggregate(Max('age'))
+    print(students8)
+    print('max age',students10)
+    
+
+    print(students)
+    print(students7)
+    print(students9)
+
+
+
+    return render(request,"orm/studentList.html",{'studentList':students})
 
