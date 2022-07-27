c2e29d16cf60b82c740c17cb0fe1dc66ce78bac5
samirvithlani <samir.vithlani83955@gmail.com>
2022-07-27 18:31:16 +0530

    mail

--- learning/learning/settings.py
+++ learning/learning/settings.py
@@ -144,3 +144,10 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/index/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pythonforsamir@gmail.com'
+EMAIL_HOST_PASSWORD = 'oysvzzarotichndt'
--- learning/news/views.py
+++ learning/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.conf import settings
+from django.core.mail import send_mail
 
 from .models import News
 
@@ -10,5 +12,11 @@ class CreateNewsView(CreateView):
     success_url = "/news/"
     model = News
     fields = "__all__"
+    
+    subject = "Test"
+    message = "Hello this mail from e-  Gift"
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['samir.vithlani83955@gmail.com','harshilmodh77@gmail.com']
+    send_mail(subject,message,email_from,recipient_list)
     
     
